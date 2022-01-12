7cf42ef0843af739c11cad9d1e3c74b2c5463b5d
samuelmmjr <samuelmmjunior@gmail.com>
2022-01-12 11:16:39 -0300

    criando conexao fron/back com axios

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,52 @@
+import React, { useState } from "react";
+import Axios from "axios";
 
 function App() {
+
+  const [values, setValues] = useState();
+
+  const handleChange = (value) => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  }
+
+  const handleClick = async () => {
+    try {
+      await Axios({
+        method: 'post',
+        url: 'http://localhost:3001/users',
+        fullname: values.fullname,
+      });
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  }
+      
+
   return (
-    <div className="App">
+    <div>
+    <form>
+      <label>
+        Nome:
+        <br />
+        <input 
+          type="name" 
+          name="fullName" 
+          placeholder='digite seu nome completo'
+          onChange={ handleChange }
+        />
+      </label>
+        <br />
+      <button 
+        type="button"
+        onClick={ handleClick }
+      >
+        Cadastrar
+      </button>
+        <br /><br />
+    </form>
     </div>
   );
 }
